ff60246eff1f038705545a8d14ba147baa6b5470
MDev123 <86262715+MDev123@users.noreply.github.com>
2023-03-04 21:24:29 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Testing Page</title>
+	<style>
+		#frame {
+			border-radius: 20px;
+			border: 2px solid #7FFF7F;
+			background-color: #000000;
+			padding: 30px;
+			margin: 50px;
+			font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+			color: #7FFF7F;
+		}
+		
+		a {
+			color: #7FFF7F;
+		}
+	</style>
+</head>
+<body style="background-color: #000000;">
+	<div id="frame">
+		<h1>Magic Is On Its Way</h1>
+		<p>This webpage is still under development. Maybe try later?</a> </p>
+	</div>
+</body>
+</html>
